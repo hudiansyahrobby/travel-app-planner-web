c36e7913f39e55ab82f9af8b4a62378b804dba5e
hudiansyahrobby <hudiansyahrobby7@gmail.com>
2024-12-12 06:42:41 +0800

    feat: add social media links and application routes constants

---
+++ src/constants/link.ts
@@ -0,0 +1,14 @@
+export const instagramLink = "https://www.instagram.com/";
+export const facebookLink = "https://www.facebook.com/";
+export const twitterLink = "https://www.twitter.com/";
+export const linkedinLink =
+  "https://www.linkedin.com/in/robby-hudiansyah-ab04641a7/";
+
+export const routes = {
+  home: "/",
+  about: "/about",
+  contact: "/contact",
+  blog: "/blog",
+  login: "/auth/login",
+  signup: "/auth/signup",
+};
