e11845f241518b1cc97a59d227f6c51605cb6f80
hudiansyahrobby <hudiansyahrobby7@gmail.com>
2024-12-12 06:20:59 +0800

    feat: add CallToAction component with dynamic icon display and animation

---
+++ src/features/section/call-to-action/config.tsx
@@ -0,0 +1,192 @@
+import Image from "next/image";
+
+export const callToActionIcons = [
+  [
+    {
+      id: "calendar",
+      icon: <Image width={62} height={62} alt="icon" src="/calendar.svg" />,
+    },
+    {
+      id: "plane",
+      icon: <Image width={62} height={62} alt="icon" src="/plane.svg" />,
+    },
+    {
+      id: "suitcase",
+      icon: <Image width={62} height={62} alt="icon" src="/suitcase.svg" />,
+    },
+    {
+      id: "globe",
+      icon: <Image width={62} height={62} alt="icon" src="/globe.svg" />,
+    },
+    {
+      id: "hotel",
+      icon: <Image width={62} height={62} alt="icon" src="/hotel.svg" />,
+    },
+    {
+      id: "compass",
+      icon: <Image width={62} height={62} alt="icon" src="/compass.svg" />,
+    },
+    {
+      id: "tent",
+      icon: <Image width={62} height={62} alt="icon" src="/tent.svg" />,
+    },
+    {
+      id: "passport",
+      icon: <Image width={62} height={62} alt="icon" src="/passport.svg" />,
+    },
+    {
+      id: "beach",
+      icon: <Image width={62} height={62} alt="icon" src="/beach.svg" />,
+    },
+    {
+      id: "landmark",
+      icon: <Image width={62} height={62} alt="icon" src="/landmark.svg" />,
+    },
+    {
+      id: "baloon",
+      icon: <Image width={62} height={62} alt="icon" src="/baloon.svg" />,
+    },
+    {
+      id: "sunset",
+      icon: <Image width={62} height={62} alt="icon" src="/sunset.svg" />,
+    },
+    {
+      id: "mountain",
+      icon: <Image width={62} height={62} alt="icon" src="/mountain.svg" />,
+    },
+    {
+      id: "camera",
+      icon: <Image width={62} height={62} alt="icon" src="/camera.svg" />,
+    },
+    {
+      id: "train",
+      icon: <Image width={62} height={62} alt="icon" src="/train.svg" />,
+    },
+  ],
+
+  [
+    {
+      id: "calendar",
+      icon: <Image width={62} height={62} alt="icon" src="/calendar.svg" />,
+    },
+    {
+      id: "plane",
+      icon: <Image width={62} height={62} alt="icon" src="/plane.svg" />,
+    },
+    {
+      id: "suitcase",
+      icon: <Image width={62} height={62} alt="icon" src="/suitcase.svg" />,
+    },
+    {
+      id: "globe",
+      icon: <Image width={62} height={62} alt="icon" src="/globe.svg" />,
+    },
+    {
+      id: "hotel",
+      icon: <Image width={62} height={62} alt="icon" src="/hotel.svg" />,
+    },
+    {
+      id: "compass",
+      icon: <Image width={62} height={62} alt="icon" src="/compass.svg" />,
+    },
+    {
+      id: "tent",
+      icon: <Image width={62} height={62} alt="icon" src="/tent.svg" />,
+    },
+    {
+      id: "passport",
+      icon: <Image width={62} height={62} alt="icon" src="/passport.svg" />,
+    },
+    {
+      id: "beach",
+      icon: <Image width={62} height={62} alt="icon" src="/beach.svg" />,
+    },
+    {
+      id: "landmark",
+      icon: <Image width={62} height={62} alt="icon" src="/landmark.svg" />,
+    },
+    {
+      id: "baloon",
+      icon: <Image width={62} height={62} alt="icon" src="/baloon.svg" />,
+    },
+    {
+      id: "sunset",
+      icon: <Image width={62} height={62} alt="icon" src="/sunset.svg" />,
+    },
+    {
+      id: "mountain",
+      icon: <Image width={62} height={62} alt="icon" src="/mountain.svg" />,
+    },
+    {
+      id: "camera",
+      icon: <Image width={62} height={62} alt="icon" src="/camera.svg" />,
+    },
+    {
+      id: "train",
+      icon: <Image width={62} height={62} alt="icon" src="/train.svg" />,
+    },
+  ],
+
+  [
+    {
+      id: "train",
+      icon: <Image width={62} height={62} alt="icon" src="/train.svg" />,
+    },
+    {
+      id: "camera",
+      icon: <Image width={62} height={62} alt="icon" src="/camera.svg" />,
+    },
+    {
+      id: "mountain",
+      icon: <Image width={62} height={62} alt="icon" src="/mountain.svg" />,
+    },
+    {
+      id: "sunset",
+      icon: <Image width={62} height={62} alt="icon" src="/sunset.svg" />,
+    },
+    {
+      id: "baloon",
+      icon: <Image width={62} height={62} alt="icon" src="/baloon.svg" />,
+    },
+    {
+      id: "landmark",
+      icon: <Image width={62} height={62} alt="icon" src="/landmark.svg" />,
+    },
+    {
+      id: "beach",
+      icon: <Image width={62} height={62} alt="icon" src="/beach.svg" />,
+    },
+    {
+      id: "passport",
+      icon: <Image width={62} height={62} alt="icon" src="/passport.svg" />,
+    },
+    {
+      id: "tent",
+      icon: <Image width={62} height={62} alt="icon" src="/tent.svg" />,
+    },
+    {
+      id: "compass",
+      icon: <Image width={62} height={62} alt="icon" src="/compass.svg" />,
+    },
+    {
+      id: "hotel",
+      icon: <Image width={62} height={62} alt="icon" src="/hotel.svg" />,
+    },
+    {
+      id: "globe",
+      icon: <Image width={62} height={62} alt="icon" src="/globe.svg" />,
+    },
+    {
+      id: "suitcase",
+      icon: <Image width={62} height={62} alt="icon" src="/suitcase.svg" />,
+    },
+    {
+      id: "plane",
+      icon: <Image width={62} height={62} alt="icon" src="/plane.svg" />,
+    },
+    {
+      id: "calendar",
+      icon: <Image width={62} height={62} alt="icon" src="/calendar.svg" />,
+    },
+  ],
+];
---
+++ src/features/section/call-to-action/index.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+
+import { callToActionIcons } from "./config";
+
+const CallToAction = () => {
+  return (
+    <section className="relative overflow-hidden">
+      <div className="relative">
+        <div className="absolute left-0 z-10 hidden h-full w-1/2 bg-[linear-gradient(to_right,hsl(var(--background))_85%,transparent_100%)] md:block"></div>
+        <div className="md:-space-x-26 container relative mx-auto flex flex-col items-start md:flex-row md:items-center">
+          <div className="bg-background z-20 -mx-[calc(theme(container.padding))] w-[calc(100%+2*theme(container.padding))] shrink-0 px-[calc(theme(container.padding))] pt-32 md:w-1/2 md:bg-transparent md:pb-32">
+            <div className="flex flex-col items-start text-left">
+              <div className="max-w-sm">
+                <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
+                  Welcome to Our World of Travel
+                </h1>
+                <Button>Get Started</Button>
+              </div>
+            </div>
+          </div>
+          <div className="h-full min-h-full w-full">
+            <div className="relative flex h-[480px] w-full flex-col gap-16 overflow-hidden pb-8 pt-12 md:py-32">
+              {callToActionIcons.map((line, i) => (
+                <div
+                  key={i}
+                  className={cn(
+                    "absolute flex gap-x-24 whitespace-nowrap odd:-translate-x-24",
+                    i % 2 === 0 ? "animate-marquee" : "animate-marquee-reverse"
+                  )}
+                  style={{ top: `${i * 10}rem` }}
+                >
+                  {line.map((integration) => (
+                    <div
+                      key={integration.id}
+                      className="border-background bg-background size-24 rounded-xl border shadow-xl"
+                    >
+                      <div className="bg-muted/20 size-full p-4">
+                        {integration.icon}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+
+              {callToActionIcons.map((line, i) => (
+                <div
+                  key={i}
+                  className={cn(
+                    "absolute top-0 ml-24 flex gap-x-24 whitespace-nowrap odd:-translate-x-24",
+                    i % 2 === 0
+                      ? "animate-marquees"
+                      : "animate-marquees-reverse"
+                  )}
+                  style={{ top: `${i * 10}rem` }}
+                >
+                  {line.map((integration) => (
+                    <div
+                      key={integration.id}
+                      className="border-background bg-background size-24 rounded-xl border shadow-xl"
+                    >
+                      <div className="bg-muted/20 size-full p-4">
+                        {integration.icon}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CallToAction;
