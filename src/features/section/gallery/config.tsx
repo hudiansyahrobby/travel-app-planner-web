8649d55ebf5a131ba4d579350a6c0b346c85fd75
hudiansyahrobby <hudiansyahrobby7@gmail.com>
2024-12-12 11:55:13 +0800

    feat: add Gallery component with carousel functionality and image data

---
+++ src/features/section/gallery/config.tsx
@@ -0,0 +1,42 @@
+export const galleryData = [
+  {
+    id: "item-1",
+    title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
+    summary:
+      "Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
+    href: "#",
+    image: "https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg",
+  },
+  {
+    id: "item-2",
+    title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
+    summary:
+      "Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
+    href: "#",
+    image: "https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg",
+  },
+  {
+    id: "item-3",
+    title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
+    summary:
+      "Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
+    href: "#",
+    image: "https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg",
+  },
+  {
+    id: "item-4",
+    title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
+    summary:
+      "Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
+    href: "#",
+    image: "https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg",
+  },
+  {
+    id: "item-5",
+    title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
+    summary:
+      "Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
+    href: "#",
+    image: "https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg",
+  },
+];
